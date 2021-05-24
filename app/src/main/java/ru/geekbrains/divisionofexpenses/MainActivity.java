0aca951df052076be6ebe5ba0e0b608056c1123e
palandarus <klarkken@yandex.ru>
2021-05-24 11:45:42 +0300

    реализации мобильного клиента для сервиса http://divisionofexpenses.herokuapp.com/doe/

--- app/src/main/java/ru/geekbrains/divisionofexpenses/MainActivity.java
+++ app/src/main/java/ru/geekbrains/divisionofexpenses/MainActivity.java
@@ -1,18 +1,15 @@
 package ru.geekbrains.divisionofexpenses;
 
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
-import android.view.Menu;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
-import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.navigation.NavigationView;
-
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.ui.AppBarConfiguration;
 
 import ru.geekbrains.divisionofexpenses.databinding.ActivityMainBinding;
 
@@ -24,42 +21,54 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        this.getWindow().getDecorView().setSystemUiVisibility(
 
-        setSupportActionBar(binding.appBarMain.toolbar);
-        binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
+
+        WebView webView = findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("http://divisionofexpenses.herokuapp.com/doe");
+
+        WebViewClient webViewClient = new WebViewClient() {
+
+            @SuppressWarnings("deprecation")
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+
+                view.loadUrl(url);
+
+                return true;
+
             }
-        });
-        DrawerLayout drawer = binding.drawerLayout;
-        NavigationView navigationView = binding.navView;
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
-                .setDrawerLayout(drawer)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
-                || super.onSupportNavigateUp();
+            @TargetApi(Build.VERSION_CODES.N)
+            @Override
+
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+
+                view.loadUrl(request.getUrl().toString());
+
+                return true;
+
+            }
+
+        };
+
+        webView.setWebViewClient(webViewClient);
+
+
+
     }
 }
